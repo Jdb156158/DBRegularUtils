7bd334a74decff42fa899436b851773e3c1225c9
jsaon <jdb156158@sina.com>
2016-04-20 16:49:02 +0800

    Create a project

---
+++ DBRegularUtils/DBRegularUtils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DBRegularUtils
+//
+//  Created by Jdb on 16/4/20.
+//  Copyright © 2016年 uimbank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DBRegularUtils/DBRegularUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DBRegularUtils
+//
+//  Created by Jdb on 16/4/20.
+//  Copyright © 2016年 uimbank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
